2c8ae51c6a8ec32068474b824ee0f398fff8da97
Bryan Hancock <bhancock@uccs.edu>
2025-04-24 16:31:22 -0600

    views.py help page final commit

--- core/views.py
+++ core/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.core.mail import send_mail
 from .models import HelpRequest, UserProfile
 import re
 
